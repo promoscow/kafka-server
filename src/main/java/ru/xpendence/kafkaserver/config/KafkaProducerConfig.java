523760cdc9f2cb802978e61b569a6ff2e334e98b
Vyacheclav Chernyshov <vyacheslav.chernyshov@stoloto.ru>
2019-02-12 16:50:00 +0300

    * config

---
+++ src/main/java/ru/xpendence/kafkaserver/config/KafkaProducerConfig.java
@@ -0,0 +1,14 @@
+package ru.xpendence.kafkaserver.config;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Author: Vyacheslav Chernyshov
+ * Date: 12.02.19
+ * e-mail: vyacheslav.chernyshov@stoloto.ru
+ */
+@Configuration
+public class KafkaProducerConfig {
+
+
+}
--- src/main/java/ru/xpendence/kafkaserver/config/KafkaTopicConfig.java
+++ src/main/java/ru/xpendence/kafkaserver/config/KafkaTopicConfig.java
@@ -1,13 +1,26 @@
 package ru.xpendence.kafkaserver.config;
 
+import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 /**
  * Author: Vyacheslav Chernyshov
  * Date: 12.02.19
- * Time: 16:27
  * e-mail: vyacheslav.chernyshov@stoloto.ru
  */
 @Configuration
 public class KafkaTopicConfig {
+
+//    @Bean
+//    public KafkaAdmin kafkaAdmin() {
+//        Map<String, Object> configs = new HashMap<>();
+//        configs.put(AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+//        return new KafkaAdmin(configs);
+//    }
+
+    @Bean
+    public NewTopic sampleTopic() {
+        return new NewTopic("sample", 1, (short) 1);
+    }
 }
